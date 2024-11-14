977847071fc0290b15b3c1759fada345891243ec
song-xudong <isxude@gmail.com>
2024-11-14 20:15:19 +0800

    Site updated: 2024-11-14 20:15:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/14/hello-world/","2024/11/14/博客/","2024/11/14/test1/"];function toRandomPost(){
+var posts=["2024/11/14/test1/","2024/11/14/hello-world/","2024/11/14/博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
