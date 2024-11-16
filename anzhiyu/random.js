fcc262afda417f0ff40ca35ba771a9a0be8e3934
song-xudong <isxude@gmail.com>
2024-11-16 12:51:32 +0800

    Site updated: 2024-11-16 12:51:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/16/论文2/","2024/11/16/论文1/","2024/11/16/转录组上游分析/"];function toRandomPost(){
+var posts=["2024/11/16/AutoDock分子对接/","2024/11/16/论文2/","2024/11/16/论文1/","2024/11/16/转录组上游分析/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
